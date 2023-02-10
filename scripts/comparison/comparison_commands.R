a7d5d2917b96f5e1f61c079954e66e8bd76b5b52
SamGa3 <106307126+SamGa3@users.noreply.github.com>
2023-02-10 18:24:56 +0100

    Add 16S vs RNA-Seq comparison

--- scripts/comparison/comparison_commands.R
+++ scripts/comparison/comparison_commands.R
@@ -28,3 +28,33 @@ rmarkdown::render("../../microbes/continuous_metadata_analysis.Rmd", params=list
 
 rm(list=ls())
 gc(full=TRUE)
+
+#---------------------------------------------------------------------------------------------------------------------------
+
+## 16S comaprison to RNA-seq values
+
+#---------------------------------------------------------------------------------------------------------------------------
+
+rm(list=ls())
+gc(full=TRUE)
+
+rmarkdown::render("../../microbes/continuous_metadata_analysis_overview.Rmd", params=list(
+  metadata1 =  c("../../metadata/IEO/metadata_16S.txt"),
+  name1 = "16S",
+  metadata2 = c("../../bacteria/RNAseq/raw/merged_unamb_score_norm/IEO/IEO_bacteria_genus_merged_unamb_score_norm.txt"),
+  name2 = "RNAseq",
+  taxa_tab = "../../bacteria/RNAseq/raw/merged_unamb_score_norm/IEO/IEO_bacteria_species_merged_unamb_score_norm.txt",
+  col_metadata = "../../metadata/IEO/IEO_clinical_metadata.txt",
+  feature1 = c("intersection"),
+  feature2 = c("intersection"),
+  thrs1 = c(0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6),
+  thrs2 = c(0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6),
+  group_plots = list(phylum=c("../../bacteria/common_bacteria_phylum_genus_RNAseq_16S_IEO_names.txt", "phylum", "genus_tax_id")),
+  output = "../../results/comparison/16S/tables/IEO_16SvsRNAseq_",
+  palette="viridis7",
+  picture_feat = "all", 
+  picture_type = "spearman"
+), output_file = "../../results/comparison/16S/IEO_correlations_16S_RNAseq.html")
+
+rm(list=ls())
+gc(full=TRUE)
