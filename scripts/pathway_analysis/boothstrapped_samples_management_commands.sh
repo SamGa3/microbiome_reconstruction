8a550561cf64ff849f8b22f985b4681648fe1937
SamGa3 <106307126+SamGa3@users.noreply.github.com>
2023-02-13 18:37:00 +0100

    Update paths

--- scripts/pathway_analysis/boothstrapped_samples_management_commands.sh
+++ scripts/pathway_analysis/boothstrapped_samples_management_commands.sh
@@ -3,10 +3,10 @@
 # left
 for i in {1..50..1}
 do
-    input_name="data/RNAseq/humann_output/random_subset_left/COAD_selectedTumor_left_55random_${i}_pathabundance.tsv" 
-    humann_split_stratified_table --input $input_name --output data/RNAseq/humann_output/random_subset_left/
-    input_name="data/RNAseq/humann_output/random_subset_left/COAD_selectedTumor_left_55random_${i}_pathabundance_unstratified.tsv" 
-    output_name="data/RNAseq/humann_output/random_subset_left/COAD_selectedTumor_left_55random_${i}_pathabundance_unstratified_cpm.tsv" 
+    input_name="data/RNAseq/humann_output/left_random/COAD_selectedTumor_left_random_${i}_pathabundance.tsv" 
+    humann_split_stratified_table --input $input_name --output data/RNAseq/humann_output/left_random/
+    input_name="data/RNAseq/humann_output/left_random/COAD_selectedTumor_left_random_${i}_pathabundance_unstratified.tsv" 
+    output_name="data/RNAseq/humann_output/left_random/COAD_selectedTumor_left_random_${i}_pathabundance_unstratified_cpm.tsv" 
     humann_renorm_table --input $input_name \
                     --output $output_name \
                     --units cpm \
@@ -16,10 +16,10 @@ done
 # right
 for i in {1..50..1}
 do
-    input_name="data/RNAseq/humann_output/random_subset_right/COAD_selectedTumor_right_72random_${i}_pathabundance.tsv" 
-    humann_split_stratified_table --input $input_name --output data/RNAseq/humann_output/random_subset_right/
-    input_name="data/RNAseq/humann_output/random_subset_right/COAD_selectedTumor_right_72random_${i}_pathabundance_unstratified.tsv" 
-    output_name="data/RNAseq/humann_output/random_subset_right/COAD_selectedTumor_right_72random_${i}_pathabundance_unstratified_cpm.tsv" 
+    input_name="data/RNAseq/humann_output/right_random/COAD_selectedTumor_right_random_${i}_pathabundance.tsv" 
+    humann_split_stratified_table --input $input_name --output data/RNAseq/humann_output/right_random/
+    input_name="data/RNAseq/humann_output/right_random/COAD_selectedTumor_right_random_${i}_pathabundance_unstratified.tsv" 
+    output_name="data/RNAseq/humann_output/right_random/COAD_selectedTumor_right_random_${i}_pathabundance_unstratified_cpm.tsv" 
     humann_renorm_table --input $input_name \
                     --output $output_name \
                     --units cpm \
@@ -29,10 +29,10 @@ done
 # CMS1
 for i in {1..50..1}
 do
-    input_name="data/RNAseq/humann_output/random_subset_CMS1/COAD_selectedTumor_CMS1_17random_${i}_pathabundance.tsv" 
-    humann_split_stratified_table --input $input_name --output data/RNAseq/humann_output/random_subset_CMS1/
-    input_name="data/RNAseq/humann_output/random_subset_CMS1/COAD_selectedTumor_CMS1_17random_${i}_pathabundance_unstratified.tsv" 
-    output_name="data/RNAseq/humann_output/random_subset_CMS1/COAD_selectedTumor_CMS1_17random_${i}_pathabundance_unstratified_cpm.tsv" 
+    input_name="data/RNAseq/humann_output/CMS1_random/COAD_selectedTumor_CMS1_random_${i}_pathabundance.tsv" 
+    humann_split_stratified_table --input $input_name --output data/RNAseq/humann_output/CMS1_random/
+    input_name="data/RNAseq/humann_output/CMS1_random/COAD_selectedTumor_CMS1_random_${i}_pathabundance_unstratified.tsv" 
+    output_name="data/RNAseq/humann_output/CMS1_random/COAD_selectedTumor_CMS1_random_${i}_pathabundance_unstratified_cpm.tsv" 
     humann_renorm_table --input $input_name \
                     --output $output_name \
                     --units cpm \
@@ -42,10 +42,10 @@ done
 # CMS234
 for i in {1..50..1}
 do
-    input_name="data/RNAseq/humann_output/random_subset_CMS234/COAD_selectedTumor_CMS234_86random_${i}_pathabundance.tsv" 
-    humann_split_stratified_table --input $input_name --output data/RNAseq/humann_output/random_subset_CMS234/
-    input_name="data/RNAseq/humann_output/random_subset_CMS234/COAD_selectedTumor_CMS234_86random_${i}_pathabundance_unstratified.tsv" 
-    output_name="data/RNAseq/humann_output/random_subset_CMS234/COAD_selectedTumor_CMS234_86random_${i}_pathabundance_unstratified_cpm.tsv" 
+    input_name="data/RNAseq/humann_output/CMS234_random/COAD_selectedTumor_CMS234_random_${i}_pathabundance.tsv" 
+    humann_split_stratified_table --input $input_name --output data/RNAseq/humann_output/CMS234_random/
+    input_name="data/RNAseq/humann_output/CMS234_random/COAD_selectedTumor_CMS234_random_${i}_pathabundance_unstratified.tsv" 
+    output_name="data/RNAseq/humann_output/CMS234_random/COAD_selectedTumor_CMS234_random_${i}_pathabundance_unstratified_cpm.tsv" 
     humann_renorm_table --input $input_name \
                     --output $output_name \
                     --units cpm \
@@ -55,10 +55,10 @@ done
 # mutation_burden_high
 for i in {1..50..1}
 do
-    input_name="data/RNAseq/humann_output/random_subset_mutation_burden_high/COAD_selectedTumor_mutation_burden_high_17random_${i}_pathabundance.tsv" 
-    humann_split_stratified_table --input $input_name --output data/RNAseq/humann_output/random_subset_mutation_burden_high/
-    input_name="data/RNAseq/humann_output/random_subset_mutation_burden_high/COAD_selectedTumor_mutation_burden_high_17random_${i}_pathabundance_unstratified.tsv" 
-    output_name="data/RNAseq/humann_output/random_subset_mutation_burden_high/COAD_selectedTumor_mutation_burden_high_17random_${i}_pathabundance_unstratified_cpm.tsv" 
+    input_name="data/RNAseq/humann_output/mutation_burden_high_random/COAD_selectedTumor_mutation_burden_high_random_${i}_pathabundance.tsv" 
+    humann_split_stratified_table --input $input_name --output data/RNAseq/humann_output/mutation_burden_high_random/
+    input_name="data/RNAseq/humann_output/mutation_burden_high_random/COAD_selectedTumor_mutation_burden_high_random_${i}_pathabundance_unstratified.tsv" 
+    output_name="data/RNAseq/humann_output/mutation_burden_high_random/COAD_selectedTumor_mutation_burden_high_random_${i}_pathabundance_unstratified_cpm.tsv" 
     humann_renorm_table --input $input_name \
                     --output $output_name \
                     --units cpm \
@@ -68,10 +68,10 @@ done
 # mutation_burden_low
 for i in {1..50..1}
 do
-    input_name="data/RNAseq/humann_output/random_subset_mutation_burden_low/COAD_selectedTumor_mutation_burden_low_95random_${i}_pathabundance.tsv" 
-    humann_split_stratified_table --input $input_name --output data/RNAseq/humann_output/random_subset_mutation_burden_low/
-    input_name="data/RNAseq/humann_output/random_subset_mutation_burden_low/COAD_selectedTumor_mutation_burden_low_95random_${i}_pathabundance_unstratified.tsv" 
-    output_name="data/RNAseq/humann_output/random_subset_mutation_burden_low/COAD_selectedTumor_mutation_burden_low_95random_${i}_pathabundance_unstratified_cpm.tsv" 
+    input_name="data/RNAseq/humann_output/mutation_burden_low_random/COAD_selectedTumor_mutation_burden_low_random_${i}_pathabundance.tsv" 
+    humann_split_stratified_table --input $input_name --output data/RNAseq/humann_output/mutation_burden_low_random/
+    input_name="data/RNAseq/humann_output/mutation_burden_low_random/COAD_selectedTumor_mutation_burden_low_random_${i}_pathabundance_unstratified.tsv" 
+    output_name="data/RNAseq/humann_output/mutation_burden_low_random/COAD_selectedTumor_mutation_burden_low_random_${i}_pathabundance_unstratified_cpm.tsv" 
     humann_renorm_table --input $input_name \
                     --output $output_name \
                     --units cpm \
