cd2f317f1df90760fd1af3c72d9a7877a235615e
SamGa3 <106307126+SamGa3@users.noreply.github.com>
2023-11-13 16:38:40 +0100

    Add single cancer type conversion

---
+++ scripts/gene_expression/TPM_conversion_commands.R
@@ -0,0 +1,123 @@
+
+#---------------------------------------------------------------------------------------------------------------------------
+
+## Create TPM from FPKM sample tables
+
+#---------------------------------------------------------------------------------------------------------------------------
+
+## COAD 
+
+rmarkdown::render("scripts/gene_expression/from_FPKM_to_TPM.Rmd", 
+  params=list(
+    manifest = "../../data/RNAseq/FPKM/tcga/COAD_fpkm_manifest.tsv",
+    dir = "../../data/RNAseq/FPKM/tcga/",
+    converter_tab = "../../data/RNAseq/FPKM/tcga/gene_annotation_v22_gene_length.txt",
+    output_fpkm = c("../../data/RNAseq/FPKM/"),
+    output_tpm = c("../../data/RNAseq/TPM/")
+  ), 
+  output_file = "../../data/RNAseq/TPM/tpm.html"
+)
+
+## GBM 
+
+rmarkdown::render("scripts/gene_expression/from_FPKM_to_TPM.Rmd", 
+  params=list(
+    manifest = "../../data/RNAseq/FPKM/tcga/GBM_fpkm_manifest.tsv",
+    dir = "../../data/RNAseq/FPKM/tcga/",
+    converter_tab = "../../data/RNAseq/FPKM/tcga/gene_annotation_v22_gene_length.txt",
+    output_fpkm = c("../../data/RNAseq/FPKM/"),
+    output_tpm = c("../../data/RNAseq/TPM/")
+  ), 
+  output_file = "../../data/RNAseq/TPM/tpm.html"
+)
+
+## LUAD 
+
+rmarkdown::render("scripts/gene_expression/from_FPKM_to_TPM.Rmd", 
+  params=list(
+    manifest = "../../data/RNAseq/FPKM/tcga/LUAD_fpkm_manifest.tsv",
+    dir = "../../data/RNAseq/FPKM/tcga/",
+    converter_tab = "../../data/RNAseq/FPKM/tcga/gene_annotation_v22_gene_length.txt",
+    output_fpkm = c("../../data/RNAseq/FPKM/"),
+    output_tpm = c("../../data/RNAseq/TPM/")
+  ), 
+  output_file = "../../data/RNAseq/TPM/tpm.html"
+)
+
+## LUSC 
+
+rmarkdown::render("scripts/gene_expression/from_FPKM_to_TPM.Rmd", 
+  params=list(
+    manifest = "../../data/RNAseq/FPKM/tcga/LUSC_fpkm_manifest.tsv",
+    dir = "../../data/RNAseq/FPKM/tcga/",
+    converter_tab = "../../data/RNAseq/FPKM/tcga/gene_annotation_v22_gene_length.txt",
+    output_fpkm = c("../../data/RNAseq/FPKM/"),
+    output_tpm = c("../../data/RNAseq/TPM/")
+  ), 
+  output_file = "../../data/RNAseq/TPM/tpm.html"
+)
+
+## HNSC 
+
+rmarkdown::render("scripts/gene_expression/from_FPKM_to_TPM.Rmd", 
+  params=list(
+    manifest = "../../data/RNAseq/FPKM/tcga/HNSC_fpkm_manifest.tsv",
+    dir = "../../data/RNAseq/FPKM/tcga/",
+    converter_tab = "../../data/RNAseq/FPKM/tcga/gene_annotation_v22_gene_length.txt",
+    output_fpkm = c("../../data/RNAseq/FPKM/"),
+    output_tpm = c("../../data/RNAseq/TPM/")
+  ), 
+  output_file = "../../data/RNAseq/TPM/tpm.html"
+)
+
+## OV 
+
+rmarkdown::render("scripts/gene_expression/from_FPKM_to_TPM.Rmd", 
+  params=list(
+    manifest = "../../data/RNAseq/FPKM/tcga/OV_fpkm_manifest.tsv",
+    dir = "../../data/RNAseq/FPKM/tcga/",
+    converter_tab = "../../data/RNAseq/FPKM/tcga/gene_annotation_v22_gene_length.txt",
+    output_fpkm = c("../../data/RNAseq/FPKM/"),
+    output_tpm = c("../../data/RNAseq/TPM/")
+  ), 
+  output_file = "../../data/RNAseq/TPM/tpm.html"
+)
+
+## READ 
+
+rmarkdown::render("scripts/gene_expression/from_FPKM_to_TPM.Rmd", 
+  params=list(
+    manifest = "../../data/RNAseq/FPKM/tcga/READ_fpkm_manifest.tsv",
+    dir = "../../data/RNAseq/FPKM/tcga/",
+    converter_tab = "../../data/RNAseq/FPKM/tcga/gene_annotation_v22_gene_length.txt",
+    output_fpkm = c("../../data/RNAseq/FPKM/"),
+    output_tpm = c("../../data/RNAseq/TPM/")
+  ), 
+  output_file = "../../data/RNAseq/TPM/tpm.html"
+)
+
+## SKCM 
+
+rmarkdown::render("scripts/gene_expression/from_FPKM_to_TPM.Rmd", 
+  params=list(
+    manifest = "../../data/RNAseq/FPKM/tcga/SKCM_fpkm_manifest.tsv",
+    dir = "../../data/RNAseq/FPKM/tcga/",
+    converter_tab = "../../data/RNAseq/FPKM/tcga/gene_annotation_v22_gene_length.txt",
+    output_fpkm = c("../../data/RNAseq/FPKM/"),
+    output_tpm = c("../../data/RNAseq/TPM/")
+  ), 
+  output_file = "../../data/RNAseq/TPM/tpm.html"
+)
+
+## BRCA 
+
+rmarkdown::render("scripts/gene_expression/from_FPKM_to_TPM.Rmd", 
+  params=list(
+    manifest = "../../data/RNAseq/FPKM/tcga/BRCA_fpkm_manifest.tsv",
+    dir = "../../data/RNAseq/FPKM/tcga/",
+    converter_tab = "../../data/RNAseq/FPKM/tcga/gene_annotation_v22_gene_length.txt",
+    output_fpkm = c("../../data/RNAseq/FPKM/"),
+    output_tpm = c("../../data/RNAseq/TPM/")
+  ), 
+  output_file = "../../data/RNAseq/TPM/tpm.html"
+)
